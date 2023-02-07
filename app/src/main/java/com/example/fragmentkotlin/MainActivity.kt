dd585ee715dfdc37ee9dad1e09b50c339b4a575b
Hamza <111977132+imquaredrs@users.noreply.github.com>
2023-02-07 16:45:14 +0300

    KotlinFragment
    Using Fragments in Kotlin

---
+++ app/src/main/java/com/example/fragmentkotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fragmentkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun firstFragment (view : View){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = BlankFragment()
+        fragmentTransaction.replace(R.id.frameLayout,firstFragment).commit()
+
+    }
+    fun secondFragment (view : View){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val secondFragment = BlankFragment2()
+        fragmentTransaction.replace(R.id.frameLayout,secondFragment).commit()
+
+    }
+}
